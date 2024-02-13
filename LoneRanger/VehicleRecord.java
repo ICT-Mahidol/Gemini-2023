984d4f762246c847844d169a6cbc52a1efb634d9
thanatsat <99607368+thanatsat@users.noreply.github.com>
2024-02-13 15:42:45 +0700

    Create VehicleRecord.java

---
+++ LoneRanger/VehicleRecord.java
@@ -0,0 +1,12 @@
+package LoneRanger.HolidayTravelVehicles;
+
+public class VehicleRecord {
+    private String vehicleSerialNumber;
+    private String name;
+    private String model;
+    private int year;
+    private String manufacture;
+    private double baseCost;
+
+    
+}
