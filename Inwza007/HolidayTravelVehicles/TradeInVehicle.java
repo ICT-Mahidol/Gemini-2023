842b4fa2ebbb6df822ac5e00e2456363e5a52e52
Kanyanut S <kanyanutsompong@gmail.com>
2024-02-13 15:36:10 +0700

    feat: add object class

---
+++ Inwza007/HolidayTravelVehicles/TradeInVehicle.java
@@ -0,0 +1,21 @@
+public class TradeInVehicle extends Vehicle{
+
+    private String make;//vehicle brand
+
+    public TradeInVehicle(int serialNo, String name, String model, int year, String manufacturer, double basePrice, String make){
+        super(serialNo, name, model, year, manufacturer, basePrice);
+        this.make=make; 
+    }
+
+    public String getMake(){
+        return this.make;
+    }
+
+    public void setMake(String make){
+        this.make=make;
+    }
+
+    public String toString(){
+        return super.toString()+this.make;
+    }
+}
