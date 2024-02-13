b59595e25902365504bca970ed3fb72d33fcbc48
rsrfay <russarin.eai@student.mahidol.edu>
2024-02-13 16:18:06 +0700

    modified:   Echo/HolidayTravelVehicles/Invoice.java

--- Echo/HolidayTravelVehicles/Invoice.java
+++ Echo/HolidayTravelVehicles/Invoice.java
@@ -1,7 +1,6 @@
 import java.util.Date;
 import java.util.ArrayList;
 
-
 public class Invoice {
     private int invoiceNumber;
     private Date invoiceDate;
@@ -17,9 +16,16 @@ public class Invoice {
     private Customer customer;
     private ArrayList<DealerInstalled> dealeroptions;
 
+    public Invoice(int invoiceNumber, Date invoiceDate, double amount, double taxes,  double licensefees,  String signature) {
+        this.invoiceNumber = invoiceNumber;
+        this.invoiceDate = invoiceDate;
+        this.amount = amount;
+        this.taxes = taxes;
+        this.licensefees = licensefees;
+        this.signature = signature;
+    }
 
-public void RequestDealerInstallOption(String request){
-    this.request = request;
+    public void RequestDealerInstallOption(String request) {
+        this.request = request;
+    }
 }
-}
-
