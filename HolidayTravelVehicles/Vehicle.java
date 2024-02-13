18cef0b916e4cd9df8399a0bc9215474e372456a
Copteez <copterkpd45@gmail.com>
2024-02-13 16:19:48 +0700

    update output
    add print

--- HolidayTravelVehicles/Vehicle.java
+++ HolidayTravelVehicles/Vehicle.java
@@ -1,12 +1,54 @@
 public class Vehicle {
-    public Vehicle(int serialNumber, String name, String model, int year, String manufacturer, double baseCost) {
-        this.serialNumber = serialNumber;
-        this.name = name;
-        this.model = model;
-        this.year = year;
-        this.manufacturer = manufacturer;
-        this.baseCost = baseCost;
 
+    private int SerialNumber;
+    private String Name;
+    private String Model;
+    private int Year;
+    private String Manufacturer;
+    private double BaseCost;
+
+    public Vehicle() {
+    }
+
+    public Vehicle(int SerialNumber, String Name, String Model, int Year, String Manufacturer, int BaseCost) {
+        this.SerialNumber = SerialNumber;
+        this.Name = Name;
+        this.Model = Model;
+        this.Year = Year;
+        this.Manufacturer = Manufacturer;
+        this.BaseCost = BaseCost;
+    }
+
+    public void setSerialNumber(int serialNumber) {
+        this.SerialNumber = serialNumber;
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    public void setModel(String Model) {
+        this.Model = Model;
+    }
+
+    public void setYear(int Year) {
+        this.Year = Year;
+    }
+
+    public void setManufacturer(String Manufacturner) {
+        this.Manufacturer = Manufacturner;
+    }
+
+    public void setBaseCost(double BaseCost) {
+        this.BaseCost = BaseCost;
+    }
+
+    public String getManufacturer() {
+        return this.Manufacturer;
+    }
+
+    public int getYear() {
+        return this.Year;
     }
 
     public int getSerialNumber() {
@@ -72,9 +114,8 @@ public class Vehicle {
         return data;
     }
 
-    public String getVehicleInfo(){
-        return
-                "Name: " + this.Name + "/nModel: " + this.Model + "/nSerial Number: " + this.SerialNumber
-                        + "/nPrice: " + (this.BaseCost);
+    public String getVehicleInfo() {
+        return "Name: " + this.Name + "\nModel: " + this.Model + "\nSerial Number: " + this.SerialNumber
+                + "\nPrice: " + (this.BaseCost);
     }
 }
