9e134ed52e6a8d950b93ee3f17ab8098188c733c
opalphaphan <phacharaphan.cha@student.mahidol.edu>
2024-02-13 16:04:55 +0700

    Done

--- Echo/HolidayTravelVehicles/Invoice.java
+++ Echo/HolidayTravelVehicles/Invoice.java
@@ -4,20 +4,19 @@ import java.util.ArrayList;
 
 
 public class Invoice {
-    private Vehicle vehicle;
-    private Vehicle tradeIn;
     private int invoiceNumber;
     private Date invoiceDate;
     private double amount;
     private double taxes;
     private double licensefees;
     private String signature;
-    private Customer customer;
     private String request;
     private ArrayList<Salesperson> Salesperson;
     private ArrayList<Vehicle> vehicles;
-    private ArrayList<Customer> vehicles;
-     
+    private Vehicle vehicle;
+    private Vehicle tradeIn;
+    private Customer customer;
+    private ArrayList<DealerInstalledOptions> dealeroptions;
 
 public void RequestDealerInstallOption(String request){
     this.request = request;
