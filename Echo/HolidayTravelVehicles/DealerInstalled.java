10ae84801d063a901be1bab322d4a857445f24a2
opalphaphan <phacharaphan.cha@student.mahidol.edu>
2024-02-13 16:12:33 +0700

    Updated

--- Echo/HolidayTravelVehicles/DealerInstalled.java
+++ Echo/HolidayTravelVehicles/DealerInstalled.java
@@ -1,11 +1,15 @@
-public class DealerInstalledOptions {
+public class DealerInstalled{
     private String optionCode;
     private String description;
     private Double price;
-
-
-public DealerInstalledOptions (String optioncode, String description, Double price ) {
-    this.optioncode = option;
 }
 
+public DealerInstalled() {
+
+}
+
+public DealerInstalled(String option, String description, Double price) {
+    this.optionCode = option;
+    this.description = description;
+    this.price = price;
 }
