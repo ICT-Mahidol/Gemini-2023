cbce89abce4da7df7061b899609e8a1f4bf28997
Copteez <copterkpd45@gmail.com>
2024-02-13 16:19:48 +0700

    update output
    add print

--- HolidayTravelVehicles/main.java
+++ HolidayTravelVehicles/main.java
@@ -1,3 +1,7 @@
 public class main {
-    Vehicle car1 = new Vehicle(55555,"v5","v8",1988,"honda",500000);
+    public static void main(String[] args) {
+        Vehicle car1 = new Vehicle(55555, "v5", "v8", 1988, "honda", 500000);
+        System.out.println(car1.getVehicleInfo());
+    }
+
 }
