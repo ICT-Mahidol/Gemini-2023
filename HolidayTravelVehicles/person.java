70ee95adbdb07168abf7c347547c7bd8866da6c6
Pattaradnai Kaeodumkoeng <kafaircoffee2434well@gmail.com>
2024-02-13 15:12:15 +0700

    Update person.java

--- HolidayTravelVehicles/person.java
+++ HolidayTravelVehicles/person.java
@@ -2,8 +2,26 @@ package HolidayTravelVehicles;
 
 public class person{
     
-    private String name;
-    private String address;
-    private int phone_number;
-    
+    protected String name;
+    protected String address;
+    protected String phone_number;
+
+    public person(String name, String address, String phone_number){
+        this.name = name;
+        this.address = address;
+        this.phone_number = phone_number;
+    }
+
+    public String getname(){
+        return this.name;
+    }
+
+    public String getaddress(){
+        return this.address;
+    }
+    public String getnumber(){
+        return this.phone_number;
+    }
 }
+
+
