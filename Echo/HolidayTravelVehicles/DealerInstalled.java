a8904d4a637be03b6bf579a204197beeee4d02a5
opalphaphan <phacharaphan.cha@student.mahidol.edu>
2024-02-13 16:07:28 +0700

    Updated error

--- Echo/HolidayTravelVehicles/DealerInstalled.java
+++ Echo/HolidayTravelVehicles/DealerInstalled.java
@@ -2,9 +2,10 @@ public class DealerInstalledOptions {
     private String optionCode;
     private String description;
     private Double price;
-}
+
 
 public DealerInstalledOptions (String optioncode, String description, Double price ) {
-    this.
+    this.optioncode = option;
 }
 
+}
