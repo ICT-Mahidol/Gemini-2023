df4f44fb50f2b98a5167a22c91f4b373c73432de
supithcha <pooppub3@gmail.com>
2024-02-13 15:39:04 +0700

    create customer ver1

---
+++ Echo/HolidayTravelVehicles/Customer.java
@@ -0,0 +1,36 @@
+public class Customer extends Person {
+    private int customerID;
+    private int membershipLevel;
+
+    public Customer() {
+    }
+
+    public Customer(String name, String address, String phone, int customerID, int membershipLevel) {
+        super();
+        this.customerID = customerID;
+        this.membershipLevel = membershipLevel;
+    }
+
+    public int getCustomerID() {
+        return customerID;
+    }
+    public void setCustomerID(int customerID) {
+        this.customerID = customerID;
+    }
+
+    public boolean negotiateVehicle(int salepersonID) {
+        if (membershipLevel >= 2) { 
+            return true; 
+        } else {
+            return false; 
+        }
+    }
+    public boolean requestTradein() {
+        if (membershipLevel >= 1) { 
+            return true;
+        } else {
+            return false; 
+        }
+    }
+
+}
