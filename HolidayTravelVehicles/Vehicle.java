ff7c95879d50ae1b7b3273791f3acdac233309e4
kikikieieiei <12816@satriwit3.ac.th>
2024-02-13 21:06:09 +0700

    Updated Vehicle.java

--- HolidayTravelVehicles/Vehicle.java
+++ HolidayTravelVehicles/Vehicle.java
@@ -72,15 +72,4 @@ public class Vehicle {
         return this.serialNumber + " " + this.name + " " + this.model + " " + this.year + " " + this.manufacturer;
     }
 
-    public static void main(String[] args) {
-        Vehicle myVehicle = new Vehicle("12345", "Toyota Camry", "XLE", 2023, "Toyota", 25000.00);
-
-        System.out.println("Serial Number: " + myVehicle.getSerialNumber());
-        System.out.println("Name: " + myVehicle.getName());
-        System.out.println("Model: " + myVehicle.getModel());
-        System.out.println("Year: " + myVehicle.getYear());
-        System.out.println("Manufacturer: " + myVehicle.getManufacturer());
-        System.out.println("Base Cost: $" + myVehicle.getBaseCost());
-    }
-
 }
