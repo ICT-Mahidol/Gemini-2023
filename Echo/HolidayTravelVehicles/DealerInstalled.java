73457e97a1efdf06e301727c7c0a9d5d1b4f829f
rurunchi <runchida.ana@student.mahidol.edu>
2024-02-13 15:54:20 +0700

    Add DealerInstalled class

---
+++ Echo/HolidayTravelVehicles/DealerInstalled.java
@@ -0,0 +1,10 @@
+public class DealerInstalledOptions {
+    private String optionCode;
+    private String description;
+    private Double price;
+}
+
+public DealerInstalledOptions (String optioncode, String description, Double price ) {
+    this.
+}
+
