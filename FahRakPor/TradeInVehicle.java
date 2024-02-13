a8c8eb7948373715f63fcacf29f0092631513004
Chutweeraya Sriwilailak <chutweraya@gmail.com>
2024-02-13 15:28:42 +0700

    add file

---
+++ FahRakPor/TradeInVehicle.java
@@ -0,0 +1,45 @@
+public class TradeInVehicle{
+    private String VehicleSerialNumber;
+    private String Name;
+    Private String Model;
+    private Year;
+    private Manufacturer;
+    private TradeInVehicle;
+
+
+    public getVehicleSerialNumber(){
+        return  this.VehicleSerialNumber;
+    }
+
+    public getName(){
+
+        return  this.Name;
+    }
+
+    public getModel(){
+        return this.Model;
+    }
+
+    public getYear(){
+        return this.year;
+    }
+
+    public getManufacturer(){
+        return this.Manufurer;
+    }
+
+    public getTradeInVehicle(){
+        return this.TradeInVehicle;
+    }
+
+    public viod setVehicleSerialNumber(String VSerialNumber){
+        this. VehicleSerialNumber=VSerialNumber;
+    }
+
+    public
+
+
+
+
+
+}
