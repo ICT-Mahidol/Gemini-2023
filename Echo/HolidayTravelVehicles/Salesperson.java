5c9389656343205d0d4ebf044c1402a860f1dff8
chaninan.phe@student.mahidol.ac.th <chaninan.phe@student.mahidol.ac.th>
2024-02-13 15:46:23 +0700

    add saleperson class

---
+++ Echo/HolidayTravelVehicles/Salesperson.java
@@ -0,0 +1,53 @@
+import java.util.ArrayList;
+
+public class Salesperson extends Person {
+    private int salespersonID;
+    private double salary;
+    private String position;
+    private ArrayList<Vehicle> vehicles;
+    private ArrayList<Invoice> invoices;
+
+    public Salesperson(String name, String address, String phone, int salespersonID, double salary, String position) {
+        super(name, address, phone);
+        this.salespersonID = salespersonID;
+        this.salary = salary;
+        this.position = position;
+        vehicles = new ArrayList<>();
+        invoices = new ArrayList<>();
+    }
+
+    public int getSalespersonID() {
+        return salespersonID;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setSalespersonID(int salespersonID) {
+        this.salespersonID = salespersonID;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public ArrayList<Invoice> createInvoices(Customer customer, Vehicle vehicle, Vehicle tradeIn, double amount, Date invoiceDate) {
+        Invoice invoice = new Invoice(customer, vehicle, tradeIn, amount, invoiceDate);
+        invoices.add(invoice);
+        return invoices;
+    }
+
+    public void negotiateVehicle(Vehicle vehicle, double price) {
+        vehicle.setPrice(price);
+    }
+}
+
