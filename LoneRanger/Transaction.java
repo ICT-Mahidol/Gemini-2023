4743befdb98176d0f96922477246289274e7ea5b
SUI ZHOU <107923460+Jeremiaha1@users.noreply.github.com>
2024-02-13 15:36:07 +0700

    Update Transaction.java

--- LoneRanger/Transaction.java
+++ LoneRanger/Transaction.java
@@ -1,2 +1,14 @@
-public class Transaction{
-  
+public class Transcation {
+    private String Customer_Sign;
+    public Transcation(String Customer_Sign) {
+        this.Customer_Sign = Customer_Sign;
+    }
+
+    public String getCustomer_Sign() {
+        return Customer_Sign;
+    }
+
+    public void setCustomer_Sign(String customer_Sign) {
+        Customer_Sign = customer_Sign;
+    }
+}
