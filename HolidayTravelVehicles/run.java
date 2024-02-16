94b52393460db12b3e60b4cbe61b1d251e900c20
MX2002MX <143877039+MX2002MX@users.noreply.github.com>
2024-02-16 20:23:33 +0700

    Create run.java

---
+++ HolidayTravelVehicles/run.java
@@ -0,0 +1,10 @@
+public class HolidayTravelVehicles {
+    public static void main(String[] args) {
+        Vehicle v = new Vehicle();
+        v.setName("Hyundai");
+        v.setModel("H1");
+        v.setYear(2012);
+        v.setBasePrice(12000000);
+        System.out.println(v.toString());
+    }
+}
