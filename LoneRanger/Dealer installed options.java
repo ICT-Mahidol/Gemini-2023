7a9b114eed182b39c759a05653da7f04938fa949
thanatsat <99607368+thanatsat@users.noreply.github.com>
2024-02-19 17:09:22 +0700

    Update Dealer installed options.java

--- LoneRanger/Dealer installed options.java
+++ LoneRanger/Dealer installed options.java
@@ -1,41 +1,46 @@
 package LoneRanger.HolidayTravelVehicles;
 
-public class DealerInstalledOptions extends VehicleRecord {
-    private String optionCode; 
-    private String description; 
-    private double price; 
-    
+public class DealerInstalledOption { // Renamed class to match the file name
+    private String optionCode;
+    private String description;
+    private double price;
 
-    public DealerInstalledOptions(String vehicleSerialNumber, String name, String model, int year, String manufacture, double baseCost, String optionCode, String description, double price) {
-        super(vehicleSerialNumber, name, model, year, manufacture, baseCost);
+    public DealerInstalledOption(String optionCode, String description, double price) {
         this.optionCode = optionCode;
         this.description = description;
         this.price = price;
     }
-    
 
-    public String getOptionCode() {
+    public String getOptionCode() { // Corrected method name to follow camelCase
         return optionCode;
     }
-    
 
-    public void setOptionCode(String optionCode) {
+    public void setOptionCode(String optionCode) { // Corrected method name to follow camelCase
         this.optionCode = optionCode;
     }
-    
-    public String getDescription() {
+
+    public String getDescription() { // Corrected method name to follow camelCase
         return description;
     }
-    
-    public void setDescription(String description) {
+
+    public void setDescription(String description) { // Corrected method name to follow camelCase
         this.description = description;
     }
-    
-    public double getPrice() {
+
+    public double getPrice() { // Corrected method name to follow camelCase
         return price;
     }
-    
-    public void setPrice(double price) {
+
+    public void setPrice(double price) { // Corrected method name to follow camelCase
         this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Option{" +
+                "optionCode='" + optionCode + '\'' +
+                ", description='" + description + '\'' +
+                ", price=" + price +
+                '}';
     }
 }
