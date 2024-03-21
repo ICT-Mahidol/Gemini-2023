2144e6b5fb6f39431ee676128c73959b4985b733
Chaiyong Ragkhitwetsagul <cragkhit@gmail.com>
2024-03-21 12:36:34 +0700

    Removed HolidayTravelVehicles.

--- HolidayTravelVehicles/main.java
+++
@@ -1,7 +0,0 @@
-public class main {
-    public static void main(String[] args) {
-        Vehicle car1 = new Vehicle(55555, "v5", "v8", 1988, "honda", 500000);
-        System.out.println(car1.getVehicleInfo());
-    }
-
-}
