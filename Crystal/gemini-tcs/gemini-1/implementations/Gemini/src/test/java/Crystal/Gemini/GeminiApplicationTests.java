edee2c79819ec9256b1a659bd3035c39cc9235e2
MX2002MX <peerapong.tat@student.mahidol.ac.th>
2024-04-21 20:46:51 +0700

    4/21 update2

---
+++ Crystal/gemini-tcs/gemini-1/implementations/Gemini/src/test/java/Crystal/Gemini/GeminiApplicationTests.java
@@ -0,0 +1,13 @@
+package Crystal.Gemini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeminiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
