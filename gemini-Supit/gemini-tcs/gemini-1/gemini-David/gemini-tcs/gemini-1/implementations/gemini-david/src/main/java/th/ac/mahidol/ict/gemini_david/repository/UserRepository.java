9bba666c70ad880be7f4f8796103665eea552183
ZISHUN GAO <144342588+Sevegoth@users.noreply.github.com>
2024-04-29 23:18:40 +0700

    Delete gemini-Supit/gemini-tcs/gemini-1/gemini-David/gemini-tcs/gemini-1 directory

--- gemini-Supit/gemini-tcs/gemini-1/gemini-David/gemini-tcs/gemini-1/implementations/gemini-david/src/main/java/th/ac/mahidol/ict/gemini_david/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package th.ac.mahidol.ict.gemini_david.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import th.ac.mahidol.ict.gemini_david.model.User;
-
-public interface UserRepository extends CrudRepository<User, Long> {
-    User findByUsername(String username);
-}
