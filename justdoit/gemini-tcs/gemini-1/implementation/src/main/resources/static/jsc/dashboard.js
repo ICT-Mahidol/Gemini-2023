020746e9fea316b71879c3dcdc309f977bfaa129
Sirawich005 <144013287+Sirawich005@users.noreply.github.com>
2024-04-29 23:47:45 +0700

    Add files via upload

---
+++ justdoit/gemini-tcs/gemini-1/implementation/src/main/resources/static/jsc/dashboard.js
@@ -0,0 +1,18 @@
+  function checkUserId() {
+    if (localStorage.getItem("userid")==0) {
+      location.replace("/");
+    }
+    if (localStorage.getItem("permission")==2) {
+      location.replace("/observerDashboard");
+    }
+    console.log(localStorage.getItem("userid"));
+  }
+  
+  window.onload = checkUserId;
+
+
+async function logout() {
+  localStorage.setItem("check_login", false);
+  localStorage.setItem("userid", 0);
+  location.replace("/");
+}
